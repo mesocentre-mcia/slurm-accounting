73dd5e9e2ad7589ad66d350167a17ff2722055c4
Pierre Gay <pierre.gay@u-bordeaux.fr>
2019-06-24 11:19:31 +0200

    add sreporting command

--- slurm_accounting/sreport.py
+++ slurm_accounting/sreport.py
@@ -1,6 +1,13 @@
 import subprocess
 import os
 import re
+import datetime
+import argparse
+
+from six import print_
+
+import config
+
 
 class Command(object):
     def __init__(self, cmd, opts=[], output_filter=lambda e: e, verbose=False, remote_host=None):
@@ -17,7 +24,7 @@ class Command(object):
             cmdlist = ['ssh', self.remote_host] + cmdlist
 
         if self.verbose:
-            print "Command:", " ".join([repr(e) for e in cmdlist])
+            print_("Command:", " ".join([repr(e) for e in cmdlist]))
 
         stdout = os.tmpfile()
         p = subprocess.Popen(cmdlist, stdout = stdout, stderr = subprocess.STDOUT)
@@ -25,7 +32,7 @@ class Command(object):
         p.wait()
 
         if p.returncode != 0:
-            print type(p.returncode)
+            print_(type(p.returncode))
             raise subprocess.CalledProcessError(cmd=" ".join(cmdlist), returncode=p.returncode)
 
         stdout.seek(0)
@@ -76,7 +83,147 @@ class SreportCluster(Command):
 
             yield r
 
-if __name__ == '__main__':
-    src = SreportCluster(verbose=True)
+class Sacct(Command):
+    @classmethod
+    def filter(cls, e):
+        return e.strip().split('|')
 
-    print '\n'.join(src('2019-01-01'))
+    def __init__(self, verbose=False, remote_host=None):
+
+        fmt = 'jobid', 'user', 'elapsed', 'ncpus', 'partition', 'nodelist', 'group', 'start', 'end', 'state'
+
+        super(Sacct, self).__init__('sacct', ['-a', '--parsable2', '--noheader', '-X',
+                                              '--format=%s' % ','.join(fmt)],
+                                             Sacct.filter, verbose=verbose,
+                                             remote_host=remote_host)
+
+        self.format=fmt
+
+    def __call__(self, start=None, end=None, partition=None, nodes=None, states=[], other_args=[]):
+        cmdline = []
+        if start is not None:
+            cmdline.append('--starttime=%s' % start)
+
+        if end is not None:
+            cmdline.append('--endtime=%s' % end)
+
+        if partition is not None:
+            cmdline.append('--partition=%s' % partition)
+
+        if nodes is not None:
+            cmdline.append('--nodelist=%s' % nodes)
+
+        if states:
+            cmdline.append('--state=%s' % ','.join(states))
+
+        cmdline += other_args
+
+        super_call = super(Sacct, self).__call__(cmdline)
+
+        for r in super_call:
+            if r is None:
+                continue
+
+            yield dict(zip(self.format, r))
+
+def parse_elapsed(s):
+    days = 0
+    hspec = s
+    if '-' in s:
+        days, hspec = s.split('-',1)
+        days = int(days)
+    hours, minutes, seconds = map(int, hspec.split(':'))
+
+    return datetime.timedelta(days=days, seconds=seconds, minutes=minutes, hours=hours)
+
+def parse_slurm_datetime(s):
+    if s == 'Unknown':
+        return None
+    return datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S")
+
+
+def parse_slurm_date(s):
+    return datetime.datetime.strptime(s, "%Y-%m-%d")
+
+
+def sreporting(conf_file, report=None, start=None, end=None):
+    cfg = config.Config(conf_file)
+
+    query_grace = parse_elapsed(cfg.get('general', 'query_grace', '00:00:00'))
+
+    query_start_date = cfg.getdate('general', 'default_start', '1970-01-01')
+    start_date = query_start_date
+    if start is not None:
+        start_date = parse_slurm_date(start)
+        query_start_date = max(query_start_date, start_date - query_grace)
+
+    query_end_date = datetime.datetime.now()
+    end_date = query_end_date
+    if end is not None:
+        end_date = parse_slurm_date(end)
+        query_end_date = end_date + query_grace
+
+    report_section = 'report:' + (report or cfg.get('general', 'default_report'))
+
+    src = Sacct(verbose=False)
+
+    partition = cfg.get(report_section, 'partition', False) or None
+    nodes = cfg.get(report_section, 'nodes', False) or None
+
+    jobs = src(start=query_start_date.strftime('%Y-%m-%dT%H:%M:%S'),
+               end=query_end_date.strftime('%Y-%m-%dT%H:%M:%S'), partition=partition, nodes=nodes,
+               states=['RUNNING'])
+
+    n = 0
+    cpuseconds = 0
+    jobs.next()
+    for r in jobs:
+        if r['state'] == 'PENDING':
+            continue
+        jstart = parse_slurm_datetime(r['start'])
+        jend = parse_slurm_datetime(r['end'])
+        if start_date and jend and jend < start_date:
+            continue
+
+        if end_date and jstart and jstart > end_date:
+            continue
+
+        jstart = max(jstart, start_date)
+        if jend is not None:
+            jend = min(jend, end_date)
+        else:
+            jend = end_date
+
+        n += 1
+#        elapsed = parse_elapsed(r['elapsed'])
+        elapsed = jend - jstart
+        cpus = int(r['ncpus'])
+        cpuseconds += elapsed.total_seconds() * cpus
+        
+        #print_(','.join([r[k] for k in src.format] + ['%.2f' % (elapsed.total_seconds()/3600)]))
+
+    print_('jobs=%d' % n, 'cpuhours=%d' % (cpuseconds / 3600), end='')
+
+    cores = cfg.get(report_section, 'cores', None)
+    if cores is not None:
+        cores = int(cores)
+        duration = (end_date - start_date).total_seconds()
+        maxseconds = cores * duration
+
+        print_(' (%.1f%%)' % (100 * cpuseconds / maxseconds), end='')
+
+    print_()
+
+def main():
+    parser = argparse.ArgumentParser(description='acconting report')
+    parser.add_argument('report', metavar='REPORT', nargs='?',
+                        default=None, help='use section [report:REPORT] '
+                        'section in configuration file')
+    parser.add_argument('-s', '--start', metavar='START_DATE',
+                        default=None, help='account jobs from START_DATE')
+    parser.add_argument('-e', '--end', metavar='END_DATE',
+                        default=None, help='account jobs up to END_DATE')
+
+    args = parser.parse_args()
+
+    sreporting('curta.conf', args.report, start=args.start, end=args.end)
